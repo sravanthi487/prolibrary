d00f55c60210813d4ad132ab28b5ab7eea1c297d
sravanthi487 <laxmiprasannapanangipalli123@gmail.com>
2021-08-24 21:37:09 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+<p align="center"><img src="https://github.com/MartMbithi/Digital_Library/blob/master/images/logo-1.png" ></p>
+
+# Digital_Library
+Digital Library is a web based and also desktop based library management system.
+
+## Login Credentials For Super User Account
+1. Email:   <b>admin@ilib.org</b><br>
+2. Password :  <b>admin</b> <br>
+
+## Login Credentials For Staff Account
+1. Username:   <b>iLib-35196</b><br>
+2. Password :  <b>demo</b> <br>
+
+## Login Credentials For Student Account
+1. Username:   <b>iLib-37508</b> or <b>student001@gmail.com</b><br>
+2. Password :  <b>demo</b> <br>
+
+
+## Glance Of The System
+<p align="center"><img src="https://github.com/MartMbithi/Digital_Library/blob/master/Screenshots/landing.png" ></p>
+<p align="center"><img src="https://github.com/MartMbithi/Digital_Library/blob/master/Screenshots/1.png" ></p>
+<p align="center"><img src="https://github.com/MartMbithi/Digital_Library/blob/master/Screenshots/2.png" ></p>
+<p align="center"><img src="https://github.com/MartMbithi/Digital_Library/blob/master/Screenshots/3.png" ></p>
+<p align="center"><img src="https://github.com/MartMbithi/Digital_Library/blob/master/Screenshots/4.png" ></p>
+<p align="center"><img src="https://github.com/MartMbithi/Digital_Library/blob/master/Screenshots/5.png" ></p>
+<p align="center"><img src="https://github.com/MartMbithi/Digital_Library/blob/master/Screenshots/6.png" ></p>
+<p align="center"><img src="https://github.com/MartMbithi/Digital_Library/blob/master/Screenshots/7.png" ></p>
+<p align="center"><img src="https://github.com/MartMbithi/Digital_Library/blob/master/Screenshots/8.png" ></p>
+
+## Theme
+
+The Digital Library Management System is crafted using [Altair-Admin-Template](https://github.com/Saworieza/Altair-Admin-Template).
+
+## License
+
+The Digital Library Management System is open-source software licensed under the [MIT license](https://opensource.org/licenses/MIT).
---
+++ staff/assets/inc/sidebar.php
@@ -0,0 +1,173 @@
+<aside id="sidebar_main">
+        
+        <div class="sidebar_main_header">
+            <div class="sidebar_logo">
+                <a href="pages_staff_dashboard.php" class="sSidebar_hide sidebar_logo_large">
+                    <img class="logo_regular" src="../sudo/assets/img/logo-1.png" alt="" height="15" width="71"/>
+                    <img class="logo_light" src="../sudo/assets/img/logo-1.png" alt="" height="15" width="71"/>
+                </a>
+                <a href="pages_staff_dashboard.php" class="sSidebar_show sidebar_logo_small">
+                    <img class="logo_regular" src="../sudo/assets/img/logo-1.png" alt="" height="32" width="32"/>
+                    <img class="logo_light" src="../sudo/assets/img/logo-1.png" alt="" height="32" width="32"/>
+                </a>
+            </div>
+        </div>
+        
+        <div class="menu_section">
+            <ul>
+                <!--Dashboard-->
+                <li title="Dashboard">
+                    <a href="pages_staff_dashboard.php">
+                        <span class="menu_icon"><i class="material-icons">&#xE871;</i></span>
+                        <span class="menu_title">Dashboard</span>
+                    </a>
+                    
+                </li>
+               
+                <!--Students-->
+                <li title="Students">
+                    <a href="#">
+                        <span class="menu_icon"><i class="material-icons">&#xE87C;</i></span>
+                        <span class="menu_title">Students</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_staff_new_student.php">Add</a></li>
+                        <li><a href="pages_staff_manage_student.php">Manage</a></li>
+                    </ul>
+                
+                </li>
+
+
+                <!--Book Inventory-->
+                <li title="Books Inventory">
+                    <a href="#">
+                        <span class="menu_icon"><i class="material-icons">&#xE8D2;</i></span>
+                        <span class="menu_title">Books Inventory</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_staff_new_book.php">Add Book</a></li>
+                        <li><a href="pages_staff_manage_books.php">Manage Books</a></li>
+                    </ul>
+                
+                </li>
+
+                <!--Library Operations-->
+                <li title="Library Operations">
+                    <a href="#">
+                        <span class="menu_icon"><i class="material-icons">&#xE8C0;</i></span>
+                        <span class="menu_title">iLibrary Operations</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_staff_new_library_book_borrow_operation.php">Borrow Book</a></li>
+                        <li><a href="pages_staff_new_library_book_return_operation.php">Return Book</a></li>
+                        <li><a href="pages_staff_library_operations_lost_book.php">Lost Books</a></li>
+                        <li><a href="pages_staff_library_operations_damanged_book.php">Damanged Books</a></li>
+                        <li><a href="pages_staff_manage_library_operations.php">Manage Operations</a></li>
+                        
+                    </ul>
+                
+                </li>
+
+                <!--Library Finances-->
+                <li title="Library Finances">
+                    <a href="#">
+                        <span class="menu_icon"><i class="material-icons">money</i></span>
+                        <span class="menu_title">iLibrary Finances</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_staff_manage_finances.php">Manage Finances</a></li>
+                    </ul>
+                
+                </li>
+
+                <!--Subscriptions-->
+                <li title="Subscriptions">
+                    <a href="#">
+                        <span class="menu_icon"><i class="material-icons">&#xE8F1;</i></span>
+                        <span class="menu_title">Subcriptions</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_staff_new_subscription.php">Add</a></li>
+                        <li><a href="pages_staff_manage_subscriptions.php">Manage</a></li>
+                    </ul>
+                
+                </li>
+
+                <!--Recomendations-->
+                <li title="Recomendantions">
+                    <a href="#">
+                        <span class="menu_icon"><i class="material-icons">&#xE1BD;</i></span>
+                        <span class="menu_title">Recomendations</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_staff_new_recomendation.php">Add</a></li>
+                        <li><a href="pages_staff_manage_reccomendations.php">Manage</a></li>
+                    </ul>
+                
+                </li>
+
+                <!--Audits-->
+                <li title="Audits">
+                    <a href="#">
+                        <span class="menu_icon"><i class="material-icons">&#xE87B;</i></span>
+                        <span class="menu_title">Audits</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_staff_audit_book_categories.php">Book Categories</a></li>
+                        <li><a href="pages_staff_audit_books_records.php">Books Records</a></li>
+                        <li><a href="pages_staff_audit_library_operations.php">Library Operations</a></li>
+                        <li><a href="pages_staff_audit_subscriptions.php">Subscription Records</a></li>
+                        <li><a href="pages_staff_audit_fines_payments.php">Fines Payment Records</a></li>
+
+                        
+                    </ul>
+                
+                </li>
+
+                <!--Library Store--
+                <li title="iLibrary Store">
+                    <a href="#">
+                        <span class="menu_icon"><i class="material-icons">&#xE8CB;</i></span>
+                        <span class="menu_title">iLibrary Store</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_sudo_new_store_item.php">New Product</a></li>
+                        <li><a href="pages_sudo_manage_store_items.php">Manage Products</a></li>
+                        <li><a href="pages_sudo_manage_orders.php">Manage Orders</a></li>
+                        <li><a href="pages_sudo_manage_payments.php">Manage Payments</a></li>
+                        <li><a href="pages_sudo_ship_purchasesed_products.php">Ship Products</a></li>
+                    </ul>
+                
+                </li>
+                -->
+                
+                <!--Mailbox-
+                This feature will be implemented on future versions               
+               
+                <li title="iLibrary Store">
+                    <a href="#">
+                    <span class="menu_icon"><i class="material-icons">&#xE158;</i></span>
+                        <span class="menu_title">Mailbox</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_staff_mail_students.php">Student Mail</a></li>
+                        <li><a href="pages_staff_manage_sendmails.php">Send Mails</a></li>
+                        <li><a href="pages_staff_manage_incommingmails.php">Inbox</a></li>
+                    </ul>
+                
+                </li>
+                -->
+
+                <!--password resets-->
+                <li title="Password Resets">
+                    <a href="#">
+                        <span class="menu_icon"><i class="material-icons">lock</i></span>
+                        <span class="menu_title">Password Resets</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_staff_manage_student_password_resets.php">Manage Students</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </aside>
